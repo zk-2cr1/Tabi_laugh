76231248339b457b9ae93d8dea51bf7eee6648b4
EC2 Default User <zk.aki.k@gmail.com>
2022-10-17 12:00:07 +0000

    [update]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -5,7 +5,7 @@ class Public::SessionsController < Devise::SessionsController
   def new_guest
     member = Member.guest
     sign_in member   # ユーザーをログインさせる
-    redirect_to mypage_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to mypage_path
   end
 
   def after_sign_in_path_for(resource)
